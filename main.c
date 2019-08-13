5d9c57971df3c42a629d723e46c244d905c612a4
fenugrec <fenugrec@users.sourceforge.net>
2019-08-13 14:46:43 -0400

    DONE
    incl round-trip test

--- main.c
+++ main.c
@@ -1,16 +1,16 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "stypes.h"
 
 //decode shit to %5d%2d%2x", serno, optnum, newval !
 
 const u8 key[]={7,7,5,5,9,8,1,4,2};
-//const u32 serno = 20377;
 
 #define TEST_SERNO 20377
 #define TEST_OPTNUM 6
-#define TEST_NEWVAL 55
+#define TEST_NEWVAL 0
 
 //reverse + invert lower 4 bits ??
 u8 mangle_38d78(u8 val) {
@@ -61,37 +61,40 @@ u8 hexdig(u8 asc) {
 
 //part of check_passwd. takes input ascii string, modifies in-place
 void decode_loop(u8 *out, u8 *ppass, u32 serno) {
-	u8 cnt = 0;
-	u8 dcnt = 8;
+	u8 cnt;
+	u8 dcnt ;
 
-	for (; cnt < 9; cnt++, dcnt--) {
+	//un-ascii
+	for (cnt = 0; cnt < 9; cnt++) {
+		ppass[cnt] = hexdig(ppass[cnt]);
+	}
+
+	for (cnt = 0, dcnt = 8; cnt < 9; cnt++, dcnt--) {
 		u8 tmp = key[cnt];
 		tmp = tmp ^ serno & 0x0F;
-		printf("dec: %02X->", ppass[cnt]);
+		//printf("dec: %02X->", ppass[cnt]);
 		ppass[cnt] ^= tmp;
 		tmp = mangle_38d78(ppass[cnt]);
 		out[dcnt] = hexdig2asc(tmp);
-		printf("%02X\n", out[dcnt]);
+		//printf("%02X\n", out[dcnt]);
 	}
 }
 
-//TODO : parity
 void encode(u8 *out, u32 serno, u8 optnum, u8 newval) {
 
 	u8 optstring[11];
 	sprintf(optstring, "%05d%02d%02x", serno, optnum, newval);
-	printf("optstring: %s\n", optstring);
 
 	u8 cnt = 0;
 	u8 dcnt = 8;
 	for (; cnt < 9; cnt++, dcnt--) {
 		u8 tmp;
-		printf("enc: %02X->", optstring[dcnt]);
+		//printf("enc: %02X->", optstring[dcnt]);
 		tmp = hexdig(optstring[dcnt]); //start from the end
 		tmp = mangle_38d78(tmp);
 		out[cnt] = tmp ^ (serno & 0x0F) ^ key[cnt];
 		out[cnt] = hexdig2asc(out[cnt]);	//conv back to asc
-		printf("%02X\n", out[cnt]);
+		//printf("%02X\n", out[cnt]);
 	}
 
 	u8 par = pwd_bitcount(out, 9) ;
@@ -99,10 +102,47 @@ void encode(u8 *out, u32 serno, u8 optnum, u8 newval) {
 	return;
 }
 
-void testshit(void) {
-	u8 tmp;
+//round-trip test varying SN
+void test_sn(void) {
 	u8 pass[11];
 	u8 shit2[11];
+
+	u8 optstring[11];
+	u32 serno;
+
+	for (serno = 0; serno < 0x3FFF; serno++) {
+		//just test enough to need 5 digits
+		sprintf(optstring, "%05d%02d%02x", serno, TEST_OPTNUM, TEST_NEWVAL);
+		encode(pass, serno, TEST_OPTNUM, TEST_NEWVAL);
+		decode_loop(shit2, pass, serno);
+		if (memcmp(shit2, optstring, 9) != 0) {
+			printf("serno mismatch : %.9s, %.9s\n", optstring, shit2);
+			break;
+		}
+	}
+}
+
+//round-trip test varying newval
+void test_newval(void) {
+	u8 pass[11];
+	u8 shit2[11];
+
+	u8 optstring[11];
+	u8 newval;
+
+	for (newval = 0; newval < 0x7F; newval++) {
+		sprintf(optstring, "%05d%02d%02X", TEST_SERNO, TEST_OPTNUM, newval);
+		encode(pass, TEST_SERNO, TEST_OPTNUM, newval);
+		decode_loop(shit2, pass, TEST_SERNO);
+		if (memcmp(shit2, optstring, 9) != 0) {
+			printf("newval mismatch : %.9s, %.9s\n", optstring, shit2);
+			break;
+		}
+	}
+}
+
+void testshit(void) {
+	u8 tmp;
 
 	//test round-trip for hex2asc2hex
 	for (tmp=0; tmp <= 0x0F; tmp++) {
@@ -121,10 +161,8 @@ void testshit(void) {
 		}
 	}
 
-	encode(pass, TEST_SERNO, TEST_OPTNUM, TEST_NEWVAL);	
-	printf("pwd : %.10s\n", pass);
-	decode_loop(shit2, pass, TEST_SERNO);
-	printf("redecoded to %.9s\n", shit2);
+	test_sn();
+	test_newval();
 	return;
 }
 
